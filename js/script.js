921ac533764ae48b1d5c1163ac43a53f8a7ab8cd
Giadagenovesi <giada.g@outlook.it>
2023-03-10 14:54:17 +0100

    Added js program

--- js/script.js
+++ js/script.js
@@ -10,4 +10,9 @@
 //  Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 
 //Creazione ciclo
-
+for (var i = 1; i <= 100; i++) {
+    if (i % 15 === 0) console.log("FizzBuzz");
+    else if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+    else console.log(i);
+}
